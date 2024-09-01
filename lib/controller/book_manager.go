f8018c09d34a500c33bf8676236d357aed388b40
Talha <mtalha7274@gmail.com>
2024-09-01 18:00:36 +0500

    register book function

--- lib/controller/book_manager.go
+++ lib/controller/book_manager.go
@@ -4,10 +4,25 @@ import (
 	"go_practice/lib/models"
 )
 
+func contains(books *[]models.Book, title string) bool {
+	for _, item := range *books {
+		if item.Title == title {
+			return true
+		}
+	}
+	return false
+}
+
 type BookManager struct {
 	AllBooks []models.Book
 }
 
-func (bm *BookManager) AddBook(book *models.Book) {
-   bm.AllBooks = append(bm.AllBooks, *book)
+func (bm *BookManager) AddBook(book *models.Book) bool {
+	bookToBeAdded := *book
+	if contains(&bm.AllBooks, bookToBeAdded.Title) {
+		return false
+	}
+
+	bm.AllBooks = append(bm.AllBooks, *book)
+	return true
 }
--- main.go
+++ main.go
@@ -4,22 +4,70 @@ import (
 	"fmt"
 	"go_practice/lib/controller"
 	"go_practice/lib/models"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
-	bookManager := controller.BookManager{}
-
-	book := models.Book{
-		Title:  "A brief history of time",
-		Author: "Stephen Hawking",
-		Pages:  100,
+	fmt.Println("WELCOME!")
+	optionsMap := map[int]string{1: "Register a book", 2: "Allocate a book", 3: "Search a book", 4: "Print all books"}
+	for {
+		for key, value := range optionsMap {
+			fmt.Println(key, value)
+		}
+		var userInput string
+		fmt.Scanln(&userInput)
+		selectedIndex, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+		switch selectedIndex {
+		case 1:
+			RegisterABook()
+		case 4:
+			PrintAllBooks()
+		}
 	}
 
-	bookManager.AddBook(&book)
+}
 
-	fmt.Println(book.Author)
+func RegisterABook() {
+	bookManager := controller.BookManager{}
 
+	var title string
+	var author string
+	var pages int
+
+	println("Write title of the book")
+	fmt.Scanln(&title)
+	println("Write author name of this book")
+	fmt.Scanln(&author)
+	println("Number of pages this book has")
+	var pagesStr string
+	fmt.Scanln(&pagesStr)
+	pages, err := strconv.Atoi(pagesStr)
+	if err != nil {
+		fmt.Println("Invalid page number")
+		return
+	}
+
+	book := models.Book{
+		Title:  title,
+		Author: author,
+		Pages:  pages,
+	}
+
+	isAdded := bookManager.AddBook(&book)
+	if !isAdded {
+		println(book.Title, "already exists!")
+		return
+	}
+
+	fmt.Println(book.Title + " is added to the collection")
+
+}
+
+func PrintAllBooks() {
+	bookManager := controller.BookManager{}
 	fmt.Printf("All Books: %+v\n", bookManager.AllBooks)
 }
